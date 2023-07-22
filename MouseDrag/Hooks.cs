bc57a8ebfca3d75ba0ebadca0ca756fd74eed4c2
woutkolkman <wout.kolkman1@hotmail.com>
2023-07-22 22:12:55 +0200

    init

---
+++ MouseDrag/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using MonoMod.RuntimeDetour;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace MouseDrag
+{
+    class Hooks
+    {
+        public static void Apply()
+        {
+			
+        }
+
+
+        public static void Unapply()
+        {
+			//TODO
+        }
+    }
+}

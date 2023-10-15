19d5c4ebce05461f5c8c499bfd32cc3b4016ce4e
woutkolkman <wout.kolkman1@hotmail.com>
2023-10-15 19:15:02 +0200

    new version

--- MouseDrag/Plugin.cs
+++ MouseDrag/Plugin.cs
@@ -9,7 +9,7 @@ using System.Security.Permissions;
 namespace MouseDrag
 {
     //also edit version in "modinfo.json"
-    [BepInPlugin("maxi-mol.mousedrag", "Mouse Drag", "0.2.7")] //(GUID, mod name, mod version)
+    [BepInPlugin("maxi-mol.mousedrag", "Mouse Drag", "0.2.8")] //(GUID, mod name, mod version)
     public class Plugin : BaseUnityPlugin
     {
         public static new ManualLogSource Logger { get; private set; } = null;

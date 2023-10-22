99ad9c99137f022e84bc4fba5860efcecf3bef86
woutkolkman <wout.kolkman1@hotmail.com>
2023-10-22 18:15:53 +0200

    workaround creature serialization exceptions

--- MouseDrag/Tools/Clipboard.cs
+++ MouseDrag/Tools/Clipboard.cs
@@ -27,8 +27,30 @@ namespace MouseDrag
             }
 
             //store player stomach object
-            if (obj is Player && (obj as Player).playerState != null)
-                (obj as Player).playerState.swallowedItem = (obj as Player).objectInStomach?.ToString();
+            if (obj is Player && (obj as Player).playerState != null) {
+                try {
+                    if ((obj as Player).objectInStomach is AbstractCreature) {
+                        AbstractCreature swallowedObj = (obj as Player).objectInStomach as AbstractCreature;
+                        if ((obj as Player).abstractCreature?.world?.GetAbstractRoom(swallowedObj.pos.room) == null)
+                            swallowedObj.pos = (obj as Player).coord;
+
+                        if (swallowedObj.world?.game?.IsStorySession == true) {
+                            (obj as Player).playerState.swallowedItem = SaveState.AbstractCreatureToStringStoryWorld(swallowedObj);
+                        } else if (swallowedObj.world?.game?.IsArenaSession == true) {
+                            //TODO you cannot retrieve a creature from string in arena, because the 
+                            //following error will occur "spawn room does not exist, creature not spawning", 
+                            //same limitation as Pokéball mod
+                            (obj as Player).playerState.swallowedItem = SaveState.AbstractCreatureToStringSingleRoomWorld(swallowedObj);
+                        } else {
+                            Plugin.Logger.LogWarning("Clipboard.CutObject, could not store swallowed creature");
+                        }
+                    } else {
+                        (obj as Player).playerState.swallowedItem = (obj as Player).objectInStomach?.ToString();
+                    }
+                } catch (Exception ex) {
+                    Plugin.Logger.LogWarning("Clipboard.CutObject exception: " + ex.ToString());
+                }
+            }
 
             Destroy.DestroyObject(obj);
         }
@@ -53,6 +75,8 @@ namespace MouseDrag
             if (Options.logDebug?.Value != false)
                 Plugin.Logger.LogDebug("PasteObject: " + apo?.ToString());
 
+            if (apo == null)
+                return;
             apo.pos = pos;
             apo.world = room.world;
             (apo as AbstractCreature)?.abstractAI?.NewWorld(room.world);
@@ -81,11 +105,28 @@ namespace MouseDrag
 
             //restore player stomach object
             if (apo.realizedObject is Player) {
-                if (String.IsNullOrEmpty((apo.realizedObject as Player).playerState?.swallowedItem)) {
-                    (apo.realizedObject as Player).objectInStomach = null;
-                } else {
-                    (apo.realizedObject as Player).objectInStomach = SaveState.AbstractPhysicalObjectFromString(apo.world, (apo.realizedObject as Player).playerState.swallowedItem);
-                    (apo.realizedObject as Player).playerState.swallowedItem = "";
+                try {
+                    if (String.IsNullOrEmpty((apo.realizedObject as Player).playerState?.swallowedItem)) {
+                        (apo.realizedObject as Player).objectInStomach = null;
+                    } else {
+                        AbstractPhysicalObject swallowedObj = null;
+                        string swallowedString = (apo.realizedObject as Player).playerState.swallowedItem;
+
+                        if (swallowedString.Contains("<oA>")) {
+                            swallowedObj = SaveState.AbstractPhysicalObjectFromString(apo.world, swallowedString);
+                        } else if (swallowedString.Contains("<cA>")) {
+                            swallowedObj = SaveState.AbstractCreatureFromString(apo.world, swallowedString, onlyInCurrentRegion: false);
+                        }
+                        if (swallowedObj != null)
+                            swallowedObj.pos = apo.pos;
+
+                        (apo.realizedObject as Player).objectInStomach = swallowedObj;
+                        if (swallowedObj == null && !String.IsNullOrEmpty(swallowedString))
+                            Plugin.Logger.LogWarning("Clipboard.PasteObject, swallowedItem string available but objectInStomach became null");
+                        (apo.realizedObject as Player).playerState.swallowedItem = "";
+                    }
+                } catch (Exception ex) {
+                    Plugin.Logger.LogWarning("Clipboard.PasteObject exception: " + ex.ToString());
                 }
             }
         }

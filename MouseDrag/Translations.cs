ad2638c7dd3e2a57025919d23ca5b7e66f529bfc
woutkolkman <wout.kolkman1@hotmail.com>
2025-04-28 18:59:30 +0200

    system for embedding <LINE> in saved language

--- MouseDrag/Translations.cs
+++ MouseDrag/Translations.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define SaveUnknownTranslations
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -80,10 +82,16 @@ namespace MouseDrag
             translationDict = new Dictionary<string, string>();
             for (int i = 0; i < file?.Length; i++) {
                 var line = file[i];
+                line = ToggleLines(line, false);
                 var parts = line?.Split(translationSeparatorChar);
                 if (parts?.Length != 2 || parts[0] == null || parts[1] == null) {
                     if (Options.logDebug?.Value != false)
-                        Plugin.Logger.LogWarning("Translations.LoadLanguage, skip invalid line in translation file: " + line);
+                        Plugin.Logger.LogWarning("Translations.LoadLanguage, skip invalid line " + i + " in translation file: " + line);
+                    continue;
+                }
+                if (translationDict.TryGetValue(parts[0], out _)) {
+                    if (Options.logDebug?.Value != false)
+                        Plugin.Logger.LogDebug("Translations.LoadLanguage, key already used at line " + i + ": " + line);
                     continue;
                 }
                 translationDict.Add(parts[0], parts[1]);
@@ -99,9 +107,47 @@ namespace MouseDrag
                 return text;
             if (translationDict.TryGetValue(text, out string translation))
                 return translation;
+#if SaveUnknownTranslations
+            translationDict.Add(text, String.Empty);
+#else
             if (Options.logDebug?.Value != false)
                 Plugin.Logger.LogDebug("Translations.Translate, no translation for text \"" + text + "\"");
+#endif
             return text;
+        }
+
+
+        public static string ToggleLines(string text, bool embed)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+            if (embed) {
+                text = text.Replace("\n", "<LINE>");
+            } else {
+                text = text.Replace("<LINE>", "\n");
+            }
+            return text;
+        }
+
+
+        public static void SaveBaseLanguage() //invoke via Dev Console
+        {
+            if (translationDict == null) {
+                if (Options.logDebug?.Value != false)
+                    Plugin.Logger.LogDebug("Translations.SaveBaseLanguage, no language was loaded");
+                return;
+            }
+            string file = "";
+            foreach (KeyValuePair<string, string> entry in translationDict)
+                file += ToggleLines(entry.Key, true) + "\n";
+            try {
+                string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                path = path.Substring(0, path.LastIndexOf(Path.DirectorySeparatorChar));
+                path += Path.DirectorySeparatorChar + "translations" + Path.DirectorySeparatorChar + "BaseLanguage";
+                File.WriteAllText(path, file);
+            } catch (Exception ex) {
+                Plugin.Logger.LogError("Translations.SaveBaseLanguage exception: " + ex?.ToString());
+            }
         }
     }
 }
